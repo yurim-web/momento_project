4e626fd0d9458274342f9fd7d379496d21810f33
yurim_web <github lyl5152@naver.com>
2026-04-13 19:21:32 +0900

    feat: Spring Boot 백엔드 설정
    - CORS 설정 (localhost:3000 허용)
    - MySQL 연결 설정 (port 8081, Momento DB)
    - Momento.java 컴파일 에러 수정
    - Maven Wrapper 실행 권한 설정

--- backup/src/main/java/me/sejoon/yurim/blog/momento/Momento.java
+++ backup/src/main/java/me/sejoon/yurim/blog/momento/Momento.java
@@ -4,17 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import me.sejoon.yurim.blog.momento.util.DB;
-
 @SpringBootApplication
 @EnableJpaAuditing
 public class Momento {
 
-    //protected static final String DB_NAME = "Momento";
-
     public static void main(String[] args) {
-        DB.check_Database(DB_NAME);
-        
         SpringApplication.run(Momento.class, args);
     }
 }
